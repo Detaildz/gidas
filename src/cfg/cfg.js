5df7544a921ad1659faffab7ff9836d9d95f93ae
Darjus <darjus.zukovski@gmail.com>
2024-04-23 22:42:00 +0300

    2

--- src/cfg/cfg.js
+++ src/cfg/cfg.js
@@ -1,7 +1,7 @@
 export const cfg = {
   API: {
     HOST: import.meta.env.PROD
-      ? 'https://gidas-api.vercel.app/trucks'
+      ? 'https://gidas-api.vercel.app'
       : 'http://localhost:3000',
   },
 };
