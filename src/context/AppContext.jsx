ddad05b49af5dd4b9756b9661ef60e9fc6444317
Darjus <darjus.zukovski@gmail.com>
2024-04-24 01:19:15 +0300

    wws

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -21,7 +21,7 @@ export const AppContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io('https://gidas-api.vercel.app/');
+    const newSocket = io('wws://gidas-api.vercel.app/');
     setSocket(newSocket);
 
     newSocket.emit('message', 'Hello, server!');
