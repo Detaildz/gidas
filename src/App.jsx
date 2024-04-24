40a970970ab3a6b5134847fbf734e88d209cd72d
Darjus <darjus.zukovski@gmail.com>
2024-04-24 22:02:52 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './reset.css';
 import './App.css';
+import io from 'socket.io-client';
 //Components
 import Main from './Components/Main/Main';
 import Header from './Components/Header/Header';
-
+const socket = io('https://gidas-api.vercel.app');
 function App() {
   return (
     <>
