87590c49112d8f16826accdcff7ac9202bde537c
Darjus <darjus.zukovski@gmail.com>
2024-04-24 18:59:13 +0300

    cfg changed back

--- src/cfg/cfg.js
+++ src/cfg/cfg.js
@@ -1,7 +1,7 @@
 export const cfg = {
   API: {
     HOST: import.meta.env.PROD
-      ? 'https://master--mano-gidas.netlify.app/'
+      ? 'https://gidas-api.vercel.app'
       : 'http://localhost:3000',
   },
 };
