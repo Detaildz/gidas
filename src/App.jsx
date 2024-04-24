ed5ce9a79b42f9b8fa833fe98c53f62eeac7e181
Darjus <darjus.zukovski@gmail.com>
2024-04-24 22:49:41 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,7 @@ import io from 'socket.io-client';
 //Components
 import Main from './Components/Main/Main';
 import Header from './Components/Header/Header';
-const socket = io.connect('https://gidas-api.vercel.app', {
-  secure: true,
-  transports: ['polling'],
-  reconnectionDelay: 500,
-  reconnection: true,
-  timeout: 5000,
-  reconnectionAttempts: Infinity,
-  autoConnect: true,
-  withCredentials: true,
-});
+const socket = io('https://gidas-api.vercel.app');
 function App() {
   console.log(socket);
   return (
